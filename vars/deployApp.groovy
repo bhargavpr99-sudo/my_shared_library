4846832e1277478f1536bb67dedee9404a3c8314
Bhargav P R <bhargavpr99@gmail.com>
2025-09-30 22:58:09 +0530

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,7 +1,14 @@
 def call(String warFile, String tomcatPath) {
-    echo "Deploying WAR to Tomcat..."
+    echo "Deploying WAR file from ${warFile} to Tomcat at ${tomcatPath}..."
 
     sh """
-        cp ${warFile} ${tomcatPath}/webapps/
+        # Stop Tomcat if running
+        sudo systemctl stop tomcat || true
+
+        # Copy WAR file
+        sudo cp ${warFile} ${tomcatPath}/
+
+        # Start Tomcat
+        sudo systemctl start tomcat
     """
 }
